0f1595fd225a1b46344b5949b0cb92c7d3b44807
Teemu Tuulari <ttuulari@gmail.com>
2015-10-17 18:00:06 +0300

    Sampling.

--- src/pancetta/core.cljs
+++ src/pancetta/core.cljs
@@ -1,23 +1,17 @@
 (ns pancetta.core
   (:require-macros [cljs.core.async.macros :refer [go go-loop]])
   (:require [cljs.nodejs :as node]
-            [cljs.core.async :refer [chan <! >! alts! timeout]]
+            [cljs.core.async :as async :refer [chan <! >! alts! timeout]]
             [pancetta.streams :as s]))
 
-(node/enable-util-print!)
-
-(defn test-debounce
+(defn test-sampling
   []
-  (let [seq-chan (s/seq->chan 100 (range 100))
-        deb      (s/debounce seq-chan 500)]
-    (s/consume deb println)))
-
-(defn test-seq
-  []
-  (let [seq-chan (s/seq->chan 10 (range 100))]
-    (s/consume seq-chan println)))
+  (let [c       (chan)
+        _       (async/onto-chan c (range 5))
+        sampled (s/sample 100 c)]
+    (s/consume println sampled)))
 
 (defn -main []
-  (test-debounce))
+  (test-sampling))
 
 (set! *main-cli-fn* -main)
--- src/pancetta/streams.cljs
+++ src/pancetta/streams.cljs
@@ -1,45 +1,28 @@
 (ns pancetta.streams
   (:require-macros [cljs.core.async.macros :refer [go go-loop]])
-  (:require [cljs.core.async :refer [chan <! >! alts! timeout close!]]
+  (:require [cljs.core.async :as async :refer [chan <! >! alts! timeout close!]]
+            [cljs.nodejs :as node]
             [cljs.core.match :refer-macros [match]])) 
 
-(defn debounce
-  [in ms]
+(node/enable-util-print!)
+
+(defn sample
+  [ms in]
   (let [out (chan)]
     (go-loop
-      [last-val nil]
-      (let [timer (timeout ms)
-            value (if (nil? last-val)
-                    (<! in)
-                    last-val)]
-        (if (nil? value)
-          (close! out)
-          (match (alts! [in timer])
-                 [_ timer]     (do (>! out value)
-                                   (recur nil))
-                 [new-val _]   (recur new-val)))))
+      [] 
+      (<! (timeout ms))
+      (let [timer  (timeout ms)]
+        (match (alts! [in timer])
+               [nil in]  (close! out)
+               [v in]    (do
+                           (>! out v)
+                           (recur))
+               [_ timer] (recur))))
     out))
 
-(defn seq->chan 
-  [ms coll]
-  (let [out (chan)]
-    (go-loop
-      [sequ values]
-      (if (empty? sequ)
-        (close! out)
-        (do
-          (<! (timeout ms))
-          (println "out")
-          (>! out (first sequ))
-          (recur (rest sequ)))))
-    out))
-
-(defn consume
-  [in fun]
-  (go-loop
-    []
-    (let [value (<! in)]
-      (if (nil? value)
-        nil
-        (do (fun value)
-            (recur))))))
+(defn consume [f c]
+  (go (loop []
+        (when-some [v (<! c)]
+          (f v)
+          (recur)))))
